4619e3a17091a2d9f7c3a03e3dedf40780537863
Stefan <stefandprins@gmail.com>
2024-06-29 18:32:36 +0200

    Updates to the sql insert logic

--- db_schema.sql
+++ db_schema.sql
@@ -7,13 +7,13 @@ USE election_results;
 -- Create the table for the provinces
 CREATE TABLE IF NOT EXISTS provinces (
     provinceID INTEGER PRIMARY KEY AUTO_INCREMENT,
-    province_name VARCHAR(255) NOT NULL
+    province_name VARCHAR(255) UNIQUE NOT NULL
 );
 
 -- Create the table for the municipalities
 CREATE TABLE IF NOT EXISTS municipalities (
     municipalityID INTEGER PRIMARY KEY AUTO_INCREMENT,
-    municipality_name VARCHAR(255) NOT NULL,
+    municipality_name VARCHAR(255) UNIQUE NOT NULL,
     provinceID INTEGER NOT NULL,
 
     -- Define the foreign
@@ -41,26 +41,31 @@ CREATE TABLE IF NOT EXISTS parties (
 
 -- Create the table for party results.
 CREATE TABLE IF NOT EXISTS party_results (
-    resultID INTEGER PRIMARY KEY AUTO_INCREMENT,
-    party_result INTEGER NOT NULL,
+    vd_numberID INTEGER,
     partyID INTEGER,
+    party_result INTEGER NOT NULL,
+    
 
     -- Define the foreign
-    FOREIGN KEY (partyID) REFERENCES parties(partyID)
-);
+    FOREIGN KEY (vd_numberID) REFERENCES voting_districts(vd_number),
+    FOREIGN KEY (partyID) REFERENCES parties(partyID),
 
-
-
--- Create the join table to link voting district with party results
-CREATE TABLE IF NOT EXISTS district_results (
-    vd_number INTEGER NOT NULL,
-    resultID INTEGER NOT NULL,
-
-
-    -- Define the foreign key
-    FOREIGN KEY (vd_number) REFERENCES voting_districts(vd_number),
-    FOREIGN KEY (resultID) REFERENCES party_results(resultID),
-    
     -- Define a composite primary key
-    PRIMARY KEY (vd_number, resultID)
+    PRIMARY KEY (vd_numberID, partyID)
 );
+
+
+
+-- -- Create the join table to link voting district with party results
+-- CREATE TABLE IF NOT EXISTS district_results (
+--     vd_number INTEGER NOT NULL,
+--     resultID INTEGER NOT NULL,
+
+
+--     -- Define the foreign key
+--     FOREIGN KEY (vd_number) REFERENCES voting_districts(vd_number),
+--     FOREIGN KEY (resultID) REFERENCES party_results(resultID),
+    
+--     -- Define a composite primary key
+--     PRIMARY KEY (vd_number, resultID)
+-- );
--- scripts/import_csv.py
+++ scripts/import_csv.py
@@ -3,11 +3,13 @@ import pandas as pd
 import mysql.connector
 from mysql.connector import Error
 
-
+# ----------------------------------------------------
+# I have code this part based on existing examples from the internet.
+# ----------------------------------------------------
 
 #  Function for reading the csv file.
 def read_csv(filepath):
-    return pd. read_csv(filepath)
+    return pd.read_csv(filepath)
 
 # Function for connecting with the database
 def db_connection(hostName, userName, userPassword, dbName):
@@ -24,26 +26,173 @@ def db_connection(hostName, userName, userPassword, dbName):
         print(f'Error: {e}')
         return None
 
+# ----------------------------------------------------
+# I have coded this part and refactored the code into its own functions.
+# ----------------------------------------------------
+
 #  Function to execute the sql queries.
-def query_execute():
-    pass
+def query_execute(cursor, sql, data=None):
+    try:
+        if sql:
+            cursor.execute(sql, data)
+        else:
+            cursor.execute(sql)
+    except Error as e:
+        print(f"Execute error occurred: {e}")
 
 # Function to commit the sql queries.
-def query_commit():
-    pass
+def query_commit(db_conn):
+    try:
+        db_conn.commit()
+    except Error as e:
+        print(f"Commit error occurred: {e}")
 
 
-# Function to return the sql query with its data from the csv file.
-def insert_province(csvfile):
+# Function to insert the province data into the table.
+def insert_province(cursor, db_conn, csvfile):
     provinces = csvfile['Province'].unique()
+
+    # Track the id of the provinces
+    province_ids = {}
 
     for province in provinces:
         print(province)
         sql = f"INSERT INTO provinces (province_name) VALUES (%s)"
 
-        cursor.execute(sql, (province,))
+        query_execute(cursor, sql, (province,))
 
-    db_conn.commit()
+        # Read the id of the province.
+        sql = "SELECT provinceID FROM provinces WHERE province_name = %s"
+
+        query_execute(cursor, sql, (province,))
+
+        # Store the province id
+        province_id = cursor.fetchone()[0]
+        province_ids[province] = province_id
+
+    query_commit(db_conn)
+
+    return province_ids
+
+
+# Function to insert the municipality data into the table.
+def insert_municipality(cursor, db_conn, csvfile, province_ids):
+    csvfile.columns = csvfile.columns.str.strip()
+    filtered = csvfile[csvfile['Municipality'].str.contains(' - ')]
+    unique_pairs = filtered[['Municipality', 'Province']].drop_duplicates().values
+
+    # Track the id of the municipalities
+    municipality_ids = {}
+
+    for municipality, province in unique_pairs:
+
+        # Read the province ID that was stored
+        province_id = province_ids[province]
+
+        # Read the municipality names but remove the prefix
+        municipality_name = municipality.split(' - ', 1)[1]
+        print(municipality_name)
+        sql = f"INSERT INTO municipalities (municipality_name, provinceID) VALUES (%s, %s)"
+
+        query_execute(cursor, sql, (municipality_name, province_id))
+
+        sql = "SELECT municipalityID FROM municipalities WHERE municipality_name = %s"
+
+        query_execute(cursor, sql, (municipality_name,))
+
+        municipality_id = cursor.fetchone()[0]
+        municipality_ids[municipality_name] = municipality_id
+
+    query_commit(db_conn)
+    return municipality_ids
+
+# Function to insert the municipality data into the table.
+def insert_voting_districts(cursor, db_conn, csvfile, municipality_ids):
+    csvfile.columns = csvfile.columns.str.strip()
+    filtered = csvfile[csvfile['Municipality'].str.contains(' - ')]
+    unique_voting_districts = filtered[['VD_Number', 'VS_Name', 'Registered_Population', 'Spoilt_Votes', 'Total_Valid_Votes', 'Municipality']].drop_duplicates()
+
+    # Track the id of the voting districts
+    voting_districts_ids = {}
+
+    for index, row in unique_voting_districts.iterrows():
+        
+        # Read the province ID that was stored
+        municipality_name = row['Municipality'].split(' - ', 1)[1]
+        municipality_id = municipality_ids[municipality_name]
+
+        # Read the municipality names but remove the prefix
+        sql = f"INSERT INTO voting_districts (vd_number, vs_name, registered_voters, spoilt_votes, total_votes, municipalityID) VALUES (%s, %s, %s, %s, %s, %s)"
+
+        query_execute(cursor, sql, (row['VD_Number'], row['VS_Name'], row['Registered_Population'], row['Spoilt_Votes'], row['Total_Valid_Votes'], municipality_id))
+
+        """ """
+        sql = "SELECT vd_number FROM voting_districts WHERE vd_number = %s"
+
+        query_execute(cursor, sql, (row['VD_Number'],))
+
+        voting_districts_id = cursor.fetchone()[0]
+        voting_districts_ids[row['VD_Number']] = voting_districts_id
+
+
+
+    query_commit(db_conn)
+    return voting_districts_ids
+
+
+# Function to insert the municipality data into the table.
+def insert_parties(cursor, db_conn, csvfile):
+    csvfile.columns = csvfile.columns.str.strip()
+    # filtered = csvfile[csvfile['Municipality'].str.contains(' - ')]
+    unique_party_name = csvfile[['sPartyName']].drop_duplicates()
+    
+    # Track the id of the voting districts
+    party_ids = {}
+
+    for index, row in unique_party_name.iterrows():
+
+        
+        # Read the municipality names but remove the prefix
+        sql = f"INSERT INTO parties (party_name) VALUES (%s)"
+
+        query_execute(cursor, sql, (row['sPartyName'],))
+
+        """ """
+        sql = "SELECT partyID FROM parties WHERE party_name = %s"
+
+        query_execute(cursor, sql, (row['sPartyName'],))
+
+        party_id = cursor.fetchone()[0]
+        party_ids[row['sPartyName']] = party_id
+
+    query_commit(db_conn)
+    return party_ids
+
+# Function to insert the results data into the table.
+def insert_results(cursor, db_conn, csvfile, voting_districts_ids, party_ids):
+    csvfile.columns = csvfile.columns.str.strip()
+
+    unique_party_results = csvfile[['VD_Number', 'sPartyName', 'Party_Votes']].drop_duplicates()
+    # print(unique_party_results.columns)
+    # print(df.columns)
+    for index, row in unique_party_results.iterrows():
+        # Read the province ID that was stored
+        vd_number_id = voting_districts_ids[row['VD_Number']]
+        party_id = party_ids[row['sPartyName']]
+
+        print(party_id)
+        
+        # Read the municipality names but remove the prefix
+        # sql = f"INSERT INTO party_results (vd_numberID, partyID, party_result) VALUES (%s, %s, %s)"
+
+        # query_execute(cursor, sql, (vd_number_id, party_id, row['Party_Votes']))
+
+
+
+    query_commit(db_conn)
+
+# ----------------------------------------------------
+
 
 # Path for the csv file
 electionResultsCSV = read_csv('data/National.csv')
@@ -60,14 +209,16 @@ db_conn = db_connection(
 if db_conn.is_connected():
     cursor = db_conn.cursor()
 
-    insert_province(electionResultsCSV)
+    # Call this function to insert the province data to the table.
+    province_ids = insert_province(cursor, db_conn, electionResultsCSV)
 
-    # provinces = electionResultsCSV['Province'].unique()
+    # Call this function to insert the municipality data to the table.
+    municipality_ids = insert_municipality(cursor, db_conn, electionResultsCSV, province_ids)
 
-    # for province in provinces:
-    #     print(province)
-    #     sql = f"INSERT INTO provinces (province_name) VALUES (%s)"
+    # Call this function to insert the voting district data to the table.
+    voting_districts_ids = insert_voting_districts(cursor, db_conn, electionResultsCSV, municipality_ids)
+    # print(voting_districts_ids)
+    # Call this function to insert the party data to the table.
+    party_ids = insert_parties(cursor, db_conn, electionResultsCSV)
 
-    #     cursor.execute(sql, (province,))
-
-    # db_conn.commit()
+    insert_results(cursor, db_conn, electionResultsCSV, voting_districts_ids, party_ids)
